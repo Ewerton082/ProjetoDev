ffa2cc845550ddea4c99bc74e679c667694dfd0b
Ewerton082 <Tec.-.Ewerton@hotmail.com>
2024-11-19 19:41:38 -0300

    Adicionando alguns Comentarios para relaização dos proximos passos

--- Bath/urls.py
+++ Bath/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from Bath.views import DailyList, CreatePetDaily
 
 urlpatterns = [
-    path('do_dia/', DailyList.as_view(), name="Diario"),
-    path('novo_pet/', CreatePetDaily.as_view(), name="novo"),
+    path('do_dia/', DailyList.as_view(), name="Diario"), #Listagem dos pets Diarios
+    path('novo_pet/', CreatePetDaily.as_view(), name="novo"), #Adicionar novo Pet Diario
 ]
 
 
-### Novo Plano, Procedimentos, deletar pet, detalhes pet, editar pet
+### Novo Plano, Procedimentos, deletar pet, detalhes pet, editar pet  [Mais importante: Realizar Relatorio de data definidia por usuario xx/xx/xxxx até xx/xx/xxxx ]
--- Storage/urls.py
+++ Storage/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', EstoqueList.as_view(), name='home_estoque'),
-    path('<int:pk>/', EstoqueDetail.as_view(), name="detail_estoque"),
-    path('nova/', CreateFood.as_view(), name='new_food'),
-    path('nova_brand/', CreateBrand.as_view(), name="new_brand"),
-    path('<int:pk>/atualizar/quantidade/', UpdateFoodAmount.as_view(), name="update_food_quantity"),
-    path('<int:pk>/atualizar/', UpdateFood.as_view(), name="update_food"),
-    path('<int:pk>/deletar/', DeleteFood.as_view(), name="delete_food")
+    path('', EstoqueList.as_view(), name='home_estoque'), #Listagem
+    path('<int:pk>/', EstoqueDetail.as_view(), name="detail_estoque"), #Detalhes de Produto
+    path('nova/', CreateFood.as_view(), name='new_food'), #Nova Ração
+    path('nova_brand/', CreateBrand.as_view(), name="new_brand"), #Nova Marca
+    path('<int:pk>/atualizar/quantidade/', UpdateFoodAmount.as_view(), name="update_food_quantity"), #Realizar vendas ou compras
+    path('<int:pk>/atualizar/', UpdateFood.as_view(), name="update_food"), # Editar Detalhes da Ração
+    path('<int:pk>/deletar/', DeleteFood.as_view(), name="delete_food") # Deletar Rações
 ]
